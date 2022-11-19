5ba3053c559d4b0db00fe585d2f135ac6e4bff35
Gabriel Hidalgo <gabriel.hid.orl@gmail.com>
2022-11-19 13:19:26 -0300

    Actualización de Clase 13

--- Leccion9/FiguraGeometrica.py
+++ Leccion9/FiguraGeometrica.py
@@ -1,7 +1,21 @@
-class FiguraGeometrica:
+
+from abc import ABC, abstractmethod
+
+#Comvertir en una clase abstracta y un decorador
+#ABC significa: abstrac Base Class, convierte una clase en abstracta
+
+class FiguraGeometrica(ABC):
     def __init__(self, ancho, alto):
-        self._ancho = ancho
-        self._alto = alto
+        if self._validar_valores(ancho):
+            self._ancho = ancho
+        else:
+            self._ancho = 0
+            print(f'Valor erroneo para el ancho: {ancho}')
+        if self._validar_valores(alto):
+            self._alto = alto
+        else:
+            self._alto = 0
+            print(f'Valor erroneo para el alto: {alto}')
 
     @property
     def ancho(self):
@@ -9,7 +23,10 @@ class FiguraGeometrica:
 
     @ancho.setter
     def ancho(self, ancho):
-        self._ancho = ancho
+        if self._validar_valores(ancho):
+            self._ancho = ancho
+        else:
+            print(f'valor error ancho: {ancho}')
 
     @property
     def alto(self):
@@ -17,7 +34,21 @@ class FiguraGeometrica:
 
     @alto.setter
     def alto(self, alto):
-        self._alto = alto
+        if self._validar_valores(alto):
+            self._alto = alto
+        else:
+            print(f'valor error ancho: {alto}')
+
+    #Para que sea obligatorio tener el metodo
+    #calcular_area en los hijos
+    @abstractmethod
+    def calcular_area(self):
+        #Se utliza pass para que no afecte al padre
+        pass
 
     def __str__(self):
         return f'FiguraGeometrica [Ancho: {self._ancho}, Alto: {self._alto}]'
+
+    def _validar_valores(self, valor): # Metodo encapsular
+        return True if 0 < valor < 10 else False
+
